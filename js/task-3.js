5468e28f6ad50146da9e6c252cb73a139bdbb51a
Ilona <ilonachorniy26@gmail.com>
2025-04-16 17:58:23 +0300

    (

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,8 @@
 const nameInput = document.querySelector("#name-input");
 nameInput.style.width = "360px";
 nameInput.style.height = "40px";
+nameInput.style.fontSize = "16px";
+
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", () => {
